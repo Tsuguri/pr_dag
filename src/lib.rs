591a740de5d5cd04c732e1ecdedb1d92b6163370
Adam Winiarczuk <adam@winiarcz.uk>
2026-04-10 10:50:04 +0200

    Turns out f32 is not hashable

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,26 @@ use dyn_hash::DynHash;
 use std::{
     collections::HashMap,
     fmt::Debug,
-    hash::{Hash, Hasher},
+    hash::{DefaultHasher, Hash, Hasher},
 };
 
 pub struct EvaluationCache {
-    entries: HashMap<u32, f32>, // TODO: replace u32 with actual hash
+    entries: HashMap<u64, f32>, // TODO: replace u32 with actual hash
 }
 
 impl EvaluationCache {
     pub fn get_or_evaluate(&mut self, node: &dyn Operation) -> f32 {
-        0.0
+        let mut hasher = DefaultHasher::new();
+        node.hash(&mut hasher);
+        let hsh = hasher.finish();
+        let result = if let Some(val) = self.entries.get(&hsh) {
+            *val
+        } else {
+            let value = node.evaluate(self);
+            self.entries.insert(hsh, value);
+            value
+        };
+        result
     }
 }
 
@@ -29,10 +39,27 @@ pub struct AddOp {
 
 impl Operation for AddOp {
     fn evaluate(&self, cache: &mut EvaluationCache) -> f32 {
-        0.0f32
+        let left = cache.get_or_evaluate(&*self.left);
+        let right = cache.get_or_evaluate(&*self.right);
+        left + right
     }
+}
+
+#[derive(Debug, Hash)]
+pub struct Leaf {
+    pub value: f32,
+}
+
+impl Operation for Leaf {
+    fn evaluate(&self, cache: &mut EvaluationCache) -> f32 {}
 }
 
 pub fn add(left: Box<dyn Operation>, right: Box<dyn Operation>) -> Box<dyn Operation> {
     Box::new(AddOp { left, right })
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn basic_add() {}
 }
